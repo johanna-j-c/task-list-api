a478685902177e069f9df79b3b953a95b1180ad1
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-05-06 22:01:26 -0400

    Fix create task endpoing. Test now passes

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,18 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at,
+        }
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(
+            title= data_dict["title"],
+            description= data_dict["description"],
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app.models.task import Task
+from app import db
 
 task_bp = Blueprint("tasks", __name__,url_prefix="/tasks")
 
@@ -7,12 +8,32 @@ task_bp = Blueprint("tasks", __name__,url_prefix="/tasks")
 def create_task():
     request_body = request.get_json()
     
-    new_task = Task(
-        title = request_body["title"],
-        description = request_body["description"],
-        completed_at = request_body["completed_at"]
-    )
+    try:
+        new_task = Task.from_dict(request_body)
+        db.session.add(new_task)
+        db.session.commit()
 
-    message = f"Task {new_task.title} successfully created"
-    return make_response(message, 201)
-        
+        return make_response({
+            "task":{
+                "id": new_task.task_id,
+                "title": new_task.title,
+                "description": new_task.description,
+                "is_complete": False
+            }}, 201)
+    
+    except KeyError as e:
+        abort(make_response({"message":f"Missing required value: {e}"}, 400))
+
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    task_list = []
+
+    for task in tasks:
+        task_list.append(dict(
+            title=task.title,
+            description=task.description,
+            completed_at=task.completed_at
+        ))
+    
+    return make_response(jsonify(task_list), 200)
