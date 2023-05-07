5e6c97d8402aa9ad7d9d334f40fb208ca9db6ae5
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-05-07 17:42:23 -0400

    Update goal and task models with one to many relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,6 +4,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
     title = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task", back_populates="goal")
 
     def to_dict(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,8 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    goal = db.relationship("Goal", back_populates="goals")
 
     def to_dict(self):
         if self.completed_at:
